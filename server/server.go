99c7631b8eb69ee80d4c8608cd4061215517cad5
sunnywhy <sunnywhy@gmail.com>
2022-01-18 00:48:07 -0500

    add server functions

--- server/server.go
+++ server/server.go
@@ -2,10 +2,27 @@ package server
 
 import (
 	"bookstore/store"
+	"bookstore/server/middleware"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
 type BookStoreServer struct {
 	s   store.Store
 	srv *http.Server
+}
+
+func NewBookStoreServer(addr string, s store.Store) *BookStoreServer {
+	srv := &BookStoreServer{
+		s: s,
+		srv: &http.Server{
+			Addr: addr,
+		},
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/book", srv.createBookHandler).Methods("POST")
+
+	srv.srv.Handler = middleware.Logging(middleware.Validating(router))
+	return srv
 }
