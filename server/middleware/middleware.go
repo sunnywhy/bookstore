ff5d0c46a74f81e96364013eadb684cb6a1a9877
sunnywhy <sunnywhy@gmail.com>
2022-01-20 10:38:28 -0500

    logging and validating middleware

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,1 +1,30 @@
 package middleware
+
+import (
+	"log"
+	"mime"
+	"net/http"
+)
+
+func Logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("recv a %s request from %s", r.Method, r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func Validating(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		contentType := r.Header.Get("Content-Type")
+		mediaType, _, err := mime.ParseMediaType(contentType)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		if mediaType != "application/json" {
+			http.Error(w, "invalid Content-Type", http.StatusUnsupportedMediaType)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
