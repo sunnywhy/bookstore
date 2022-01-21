f0dfc94673e2e081aa43a630559d360d6f55a590
sunnywhy <sunnywhy@gmail.com>
2022-01-21 16:50:22 -0500

    make it work

--- server/server.go
+++ server/server.go
@@ -1,9 +1,13 @@
 package server
 
 import (
-	"bookstore/store"
 	"bookstore/server/middleware"
+	"bookstore/store"
+	"context"
+	"encoding/json"
 	"net/http"
+	"time"
+
 	"github.com/gorilla/mux"
 )
 
@@ -22,7 +26,61 @@ func NewBookStoreServer(addr string, s store.Store) *BookStoreServer {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/book", srv.createBookHandler).Methods("POST")
+	router.HandleFunc("/book/{id}", srv.updateBookHandler).Methods("POST")
+	router.HandleFunc("/book/{id}", srv.getBookHandler).Methods("GET")
+	router.HandleFunc("/book", srv.getAllBookHandler).Methods("GET")
+	router.HandleFunc("/book/{id}", srv.delBookHandler).Methods("DELETE")
 
 	srv.srv.Handler = middleware.Logging(middleware.Validating(router))
 	return srv
+}
+
+func (bs *BookStoreServer) ListenAndServe() (<-chan error, error) {
+	var err error
+	errChan := make(chan error)
+	go func() {
+		err = bs.srv.ListenAndServe()
+		errChan <- err
+	}()
+
+	select {
+	case err = <-errChan:
+		return nil, err
+	case <-time.After(time.Second):
+		return errChan, nil
+	}
+}
+
+func (bs *BookStoreServer) Shutdown(ctx context.Context) error {
+	return bs.srv.Shutdown(ctx)
+}
+
+func (bs *BookStoreServer) createBookHandler(w http.ResponseWriter, req *http.Request){
+	decode := json.NewDecoder(req.Body)
+	var book store.Book
+	if err := decode.Decode(&book); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := bs.s.Create(&book); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+func (bs *BookStoreServer) getBookHandler(w http.ResponseWriter, req *http.Request) {
+	// id, ok := mux.Vars(req)["id"]
+}
+
+func (bs *BookStoreServer) updateBookHandler(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (bs *BookStoreServer) getAllBookHandler(w http.ResponseWriter, req *http.Request) {
+	
+}
+
+func (bs *BookStoreServer) delBookHandler(w http.ResponseWriter, req *http.Request) {
+
 }
